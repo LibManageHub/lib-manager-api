831ea37a562115342bb6db31ccbe6eff7f7dbed3
sokolovgit <sanok.sokolov928100@gmail.com>
2024-07-08 17:25:39 +0300

    created main directories and swagger set up

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+
+  const options = new DocumentBuilder()
+    .setTitle('Library Manager API')
+    .setDescription('The Library Manager API description')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Swagger is running on: http://localhost:${port}/api`);
+}
+
+bootstrap();
