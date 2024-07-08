c50c943218db19736e4b752733a13dd98b6df622
sokolovgit <sanok.sokolov928100@gmail.com>
2024-07-08 18:53:08 +0300

    replaced default express with fastify

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   const logger = new Logger();
 
   const options = new DocumentBuilder()
     .setTitle('Library Manager API')
     .setDescription('The Library Manager API description')
     .setVersion('1.0')
-    .setBasePath('api')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
-  logger.log(`Application is running on: http://localhost:${port}`);
-  logger.log(`Swagger is running on: http://localhost:${port}/api`);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+  logger.log(`Swagger is running on: ${await app.getUrl()}/api`);
 }
 
 bootstrap();
